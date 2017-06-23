ee20623611c9c3ccab62c043093146f87be51cec
Diego Pineda <dpineda@nullhappens.com>
2017-06-22 23:09:26 -0400

    added changeset casting and validation

--- apps/rsvp/lib/events/events.ex
+++ apps/rsvp/lib/events/events.ex
@@ -1,6 +1,8 @@
 defmodule Rsvp.Events do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "events" do
     field :title, :string
     field :location, :string
@@ -8,4 +10,22 @@ defmodule Rsvp.Events do
     field :description, :string
     timestamps()
   end
+
+  @required_fields ~w(title location date)a
+  @optional_fields ~w(description)a
+
+  def changeset(event, params \\ %{}) do
+    event
+      |> cast(params, @required_fields ++ @optional_fields)
+      |> validate_required(@required_fields)
+      |> validate_change(:date, &must_be_future/2)
+  end
+
+  def must_be_future(_, value) do
+    Ecto.DateTime.compare(value, Ecto.DateTime.utc)
+      |> get_error
+  end
+
+  defp get_error(comparison) when comparison == :lt, do: [date: "Cannot be in the past"]
+  defp get_error(_), do: []
 end
