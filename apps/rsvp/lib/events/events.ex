964ffda8c7baa30f3e4acbd2665209b19d387a42
Diego Pineda <dpineda@nullhappens.com>
2017-06-22 21:59:18 -0400

    added migrations for event tables

---
+++ apps/rsvp/lib/events/events.ex
@@ -0,0 +1,11 @@
+defmodule Rsvp.Events do
+  use Ecto.Schema
+
+  schema "events" do
+    field :title, :string
+    field :location, :string
+    field :date, Ecto.DateTime
+    field :description, :string
+    timestamps()
+  end
+end
