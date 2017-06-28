11cc0b2fb21a7ca10e38b360af852a6529c4d3cd
Diego Pineda <dpineda@nullhappens.com>
2017-06-27 22:14:38 -0400

    added database seeding scripts and tasks

---
+++ apps/rsvp/priv/repo/seed.exs
@@ -0,0 +1,4 @@
+unless(Rsvp.EventQueries.any) do
+  Rsvp.EventQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{date: "2017-07-20 08:00:00", title: "Charles Spurgeon's Birthday", location: "London"}))
+  Rsvp.EventQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{date: "2017-07-23 08:00:00", title: "Summer Codefest", location: "Omaha, NE"}))
+end
